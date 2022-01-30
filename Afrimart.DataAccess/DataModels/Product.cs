34d4824620c4ee671c8aac59a1552b97e216f29d
Sodiq Yusuff <syusuff@wsits.com>
2022-01-30 16:49:39 -0500

    add cartzilla template files for vendor dashboard

--- Afrimart.DataAccess/DataModels/Product.cs
+++ Afrimart.DataAccess/DataModels/Product.cs
@@ -10,10 +10,32 @@ namespace Afrimart.DataAccess.DataModels
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal UnitPrice { get; set; } // will be price per weight for weightable products
+
+        /// <summary>
+        /// This is the price that the item is sold for at any time, discounted or not
+        /// So, selling price = unitPrice when no sale, and = unit-discount when on sale
+        /// selling price must already include PLATFORM FEE (certain percentage out of selling price)
+        /// </summary>
+        public decimal SellingPrice { get; set; }
+
+        // WHEN ON SALE
+        // display the original price (base plus fee)
+        // display the discounted price ((base - discount) plus fee)
+
+
+
+        // WHEN NO SALE
+        // display the original price (base price plus fee)
+        
+
+
+
         public bool IsOnSale { get; set; }
+        // we can keep the % as float and display as int
+        public double DiscountPercentage { get; set; } // save discount value of percentage?
         // may put the below in another class
-        public DiscountType DiscountType { get; set; }  // set this when product is added or updated?
-        public decimal DiscountValue { get; set; }
+        // public DiscountType DiscountType { get; set; }  // set this when product is added or updated?
+
 
         public ProductCategory Category { get; set; }
 
