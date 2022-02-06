1e36fff0efb5f6d0f7ef428fb5a7cc8684083675
Sodiq Yusuff <syusuff@wsits.com>
2022-02-05 21:45:08 -0500

    homepage styling

--- Afrimart.Dto/Stores/AddProductRequestDto.cs
+++ Afrimart.Dto/Stores/AddProductRequestDto.cs
@@ -9,7 +9,7 @@ namespace Afrimart.Dto.Stores
     {
         [Required]
         [RegularExpression("^[-A-Za-z0-9 ,_'=.%]+$", ErrorMessage = "Only alphabets and a few specials allowed")]
-        [StringLength(10, ErrorMessage = "{0} cannot be more than {1} characters")]
+        [StringLength(100, ErrorMessage = "{0} cannot be more than {1} characters")]
         public string Name { get; set; }
 
         [Required]
